86b21e6bd1b34ad166747ecbf16a07e5e8045945
predrag jandric <predrag.jandric.bg@gmail.com>
2024-08-31 23:29:47 +0200

    if applied, will ADD: and connect redux store to the app and use it in Customer.js as an example

---
+++ src/features/customers/Customer.js
@@ -0,0 +1,12 @@
+import { useSelector } from "react-redux";
+
+function Customer() {
+ const customer = useSelector((store) => store.fullName);
+
+ console.log(customer);
+ 
+
+  return <h2>👋 Welcome, {customer}</h2>;
+}
+
+export default Customer;
